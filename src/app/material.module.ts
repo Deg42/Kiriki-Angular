8a9cda4488793dd4a108fd1fcdd0b327cd5e8fe8
Juan Antonio González <73712866+Deg42@users.noreply.github.com>
2022-04-26 21:53:12 +0200

    login form updated

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const myModules = [
   MatToolbarModule,
@@ -12,7 +15,11 @@ const myModules = [
   MatButtonModule,
   MatMenuModule,
   MatListModule,
-  MatIconModule
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
 ];
 
 @NgModule({
